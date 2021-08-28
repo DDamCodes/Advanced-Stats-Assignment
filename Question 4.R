1d7ddf4d28022f0d23cb253de49d81b90fbb6bed
miss-francis <66588531+miss-francis@users.noreply.github.com>
2021-08-28 13:17:45 -0700

    Create Question 4.R

---
+++ Question 4.R
@@ -0,0 +1,37 @@
+#change the directory to where the data resides.
+setwd("c:/users/sxf190018/documents/buan6359/assignment")
+
+#clear environment
+rm(list = ls())
+
+#fetch the data into variable
+olympics <- readRDS("OlympicsMedal.rds")
+
+#find the starting year from the table.
+StartYear <- head(sort(olympics$Year), 1)
+
+#find the end year from the graph.
+EndYear <- tail(sort(olympics$Year), 1)
+
+#time series graph for USA.
+USATS <- ts(olympics$`United States`,
+            start = StartYear,
+            end = EndYear,
+            frequency = 1
+)
+
+#time series graph for Canada.
+CanadaTS <- ts(olympics$Canada,
+               start = StartYear,
+               end = EndYear,
+               frequency = 1
+)
+
+#plot both ts into single graph.
+ts.plot(USATS, CanadaTS,
+        main = "Time Series Graph: Olympic Medal",
+        ylab = "No of medals obtained",
+        xlab = "Year",
+        col = c("blue", "red"),
+        lty = c(1)
+)
