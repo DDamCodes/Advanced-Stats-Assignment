5bfa602a91bda11d94cc5745bd890960007117c9
DDam <dam.debjeet@gmail.com>
2021-09-06 20:54:26 -0500

    Update Question 4.R
    Final changes to time series

--- Question 4.R
+++ Question 4.R
@@ -7,31 +7,31 @@ rm(list = ls())
 #include this library
 library("dplyr")
 
-#fetch the data into variable
+#read file
 olympics <- readRDS("OlympicsMedal.rds")
 
 #find the starting year from the table.
-StartYear <- head(sort(olympics$Year), 1)
+StartYear <- min(olympics$Year)
 
 #find the end year from the graph.
-EndYear <- tail(sort(olympics$Year), 1)
+EndYear <- max(olympics$Year)
 
 #time series graph for USA.
 USATS <- ts(olympics$`United States`,
             start = c(1924),
-            end = c(2014),
-            frequency = 0
+            #end = c(2014),
+            frequency = 0.25
 )
 
 #time series graph for Canada.
-# CanadaTS <- ts(olympics$Canada,
-#                start = c(1924, 1),
-#                end = c(2014, 12),
-#                frequency = 1
-# )
+CanadaTS <- ts(olympics$Canada,
+               start = c(1924),
+               #end = c(2014),
+               frequency = 0.25
+)
 
 #plot both ts into single graph.
-ts.plot(USATS,
+ts.plot(USATS, CanadaTS,
         main = "Time Series Graph: Olympic Medal",
         ylab = "No of medals obtained",
         xlab = "Year",
@@ -39,7 +39,6 @@ ts.plot(USATS,
         type="l",
         lty = c(1)
 )
-
 legend('topleft',
        legend = c("USA","Canada"),
        col = c("blue","red"),
