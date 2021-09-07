f2e5663288ac6d26a3cd8c9b1f965b16f595e879
miss-francis <66588531+miss-francis@users.noreply.github.com>
2021-09-06 20:15:22 -0700

    Update Assignment 1.R

--- Assignment 1.R
+++ Assignment 1.R
@@ -105,11 +105,11 @@ with(MBA,table(Degreelabel,University)) %>%
   prop.table(margin=) %>% #default is "margin=", which calculates proportions over the entire table.
   round(2)
 
-#----------Graphing the Relationship b/w two Nominal Variables------------------
+#Graphing the Relationship b/w two Nominal Variables
 #Comparison Bar Graph
 count<- table(MBA$Degreelabel,MBA$University); count
 barplot(count, 
-        main="Two-dimentional Bar Chart",
+        main="Two-dimensional Bar Chart",
         xlab="University", 
         ylab="Frequency", 
         col=c("darkblue","red","green", "yellow"),
@@ -130,15 +130,16 @@ GSS2014 <- readRDS("GSS2014.rds")
 #get a subset of relevant data.
 GSS2014sub <- subset(GSS2014, select = c(CASEID, AGE, EDUC))
 
+#Calculating the minimum and maximum years of education
 EDUCLow = min(GSS2014sub$EDUC, na.rm = TRUE)
 EDUCHigh = max(GSS2014sub$EDUC, na.rm = TRUE)
 
-par(mar = c(4, 4, 6, 4))
+par(mar = c(6, 6, 6, 4))
 #plotting histogram.
 hist(GSS2014sub$EDUC, 
      breaks = 10,
      freq = T,
-     main = "Histogram of Ameriacan adult's Education in 2014",
+     main = "Histogram of American adult's Education in 2014",
      xlab = "Education in Years",
      ylab = "Frequency",
      labels = T, #label the values
@@ -152,14 +153,13 @@ hist(GSS2014sub$EDUC,
  
 #--------------------------------QUESTION 4------------------------------------#
 
-
 #clear environment
 rm(list = ls())
 
 #include this library
 library("dplyr")
 
-#read the data into variable.
+#fetch the data into variable
 olympics <- readRDS("OlympicsMedal.rds")
 
 #find the starting year from the table.
@@ -168,25 +168,23 @@ StartYear <- min(olympics$Year)
 #find the end year from the graph.
 EndYear <- max(olympics$Year)
 
-#time series graph for USA.
 USATS <- ts(olympics$`United States`,
             start = c(1924),
-            #end = c(2014),
             frequency = 0.25
 )
 
 #time series graph for Canada.
 CanadaTS <- ts(olympics$Canada,
-               start = c(1924),
-               #end = c(2014),
-               frequency = 0.25
+                start = c(1924),
+                frequency = 0.25
 )
 
-#plot both ts into single graph.
+#plot both ts into single time series graph.
 ts.plot(USATS, CanadaTS,
         main = "Time Series Graph: Olympic Medal",
         ylab = "No of medals obtained",
         xlab = "Year",
+        xlim = c(StartYear, EndYear),
         col = c("blue", "red"),
         type="l",
         lty = c(1)
@@ -196,7 +194,6 @@ legend('topleft',
        col = c("blue","red"),
        lwd = 1,
        lty = 1,
-       bty = "n",
        cex = 0.7)
 
 
